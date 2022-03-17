84e5fb15912185e835d4f1a4a29e55681650e704
shivdvaio <57656560+shivdvaio@users.noreply.github.com>
2022-03-17 20:50:24 +0530

    assignment 1 complete

--- node_course/routes.js
+++ node_course/routes.js
@@ -1,39 +1,55 @@
 const fs = require('fs');
-const requestHandler = (req, res, ) => {
-const url = req.url;
-const method = req.method;
 
-    if (url === "/") {
-        res.write(
-            '<body><form action="/message" method="POST"> <input type ="text" name = "message"><button type = "submit">Send</button></form> </body>'
-        );
+const requestHandler = (req, res) => {
+    const url = req.url;
 
-        return res.end();
-    }
+    switch (url) {
 
-    if (url === "/message" && req.method === "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
+        case '/': {
 
-        });
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Assignment 1</title></head>');
+            res.write(
+                '<body><form action="/createUsers" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+            );
+            res.write('</html>');
+            
+            return res.end();
 
+        }
 
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync("message.txt", message);
-            res.write("<h1>File is written</h1>");
+        case '/users': {
+
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Assignment 1</title></head>');
+            res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+            res.write('</html>');
+            return res.end();
+        }
+
+        case '/createUsers': {
+            const body = [];
+            req.on('data', chunk => {
+                body.push(chunk);
+            });
+
+            req.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
+            });
 
             res.statusCode = 302;
+            res.setHeader('Location', '/');
+            res.end();
 
-
-        });
-
+        }
 
     }
 
 
 }
+
 
 module.exports = requestHandler;
